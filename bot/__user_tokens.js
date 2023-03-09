80da04ff5d1132f6f77f63cb06a03efb3d2c6317
NikkieDev <dev.nikkie@gmail.com>
2023-03-09 20:56:13 +0100

    commit

---
+++ bot/__user_tokens.js
@@ -0,0 +1,20 @@
+const mc = require('mongodb').MongoClient;
+const conf = require('./private/config.json');
+
+async function withdraw(user, amount) {
+
+}
+
+async function balanceCheck(user) {
+
+}
+
+async function gift(user, target, amount) {
+    
+}
+
+module.exports = {
+    withdraw,
+    balanceCheck,
+    gift
+}
---
+++ bot/users.js
@@ -0,0 +1,25 @@
+const mc = require('mongodb').MongoClient;
+const conf = require('./private/config.json');
+
+async function setPronouns(user, pronouns) {
+    
+}
+
+async function setGender(user, gender) {
+
+}
+
+async function setName(user, name) {
+
+}
+
+async function setPartner(user, target) {
+
+}
+
+module.exports = {
+    setPronouns,
+    setGender,
+    setName,
+    setPartner
+}
